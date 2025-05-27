73064b23766dd3ff8c634412a9c76a1ac4dac15a
Maeva108 <makuetchemaeva9@gmail.com>
2025-05-27 17:23:47 +0100

    test coverage report

---
+++ tests/test_coverage_report.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_example_1():
+    assert 1 + 1 == 2
+
+def test_example_2():
+    assert 2 * 2 == 4
+
+def test_example_3():
+    assert 3 - 1 == 2
+
+def test_example_4():
+    assert 4 / 2 == 2
+
+def test_example_5():
+    assert 5 % 2 == 1
